69669f329e22b2e53f2044a47a338ee89579f622
huanghongyi <121650447@qq.com>
2023-12-03 10:34:10 +0800

    feat: block3

--- calendar.vue
+++ calendar.vue
@@ -1,12 +1,78 @@
 <template>
-  <el-calendar v-model="value" />
-  <el-button type="primary" @click="changeDate">改变日期</el-button>
+  <el-form :model="form" label-width="120px">
+    <el-form-item label="Activity name">
+      <el-input v-model="form.name"/>
+    </el-form-item>
+    <el-form-item label="Activity zone">
+      <el-select v-model="form.region" plcaeholder="please select your zone">
+        <el-option label="Zone one" value="shanghai" />
+        <el-option label="Zone two" value="beijing" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="Activity time">
+      <el-col :span="11">
+        <el-date-picker
+          v-model="form.date1"
+          type="date"
+          placeholder="Pick a date"
+          style="width: 100%"
+         />
+      </el-col>
+      <el-col :span="2" class="text-center">
+        <span class="text-gray-500">-</span>
+      </el-col>
+      <el-col :span="11">
+        <el-time-picker
+          v-model="form.date2"
+          type="date"
+          placeholder="Pick a time"
+          style="width: 100%"
+         />
+      </el-col>
+    </el-form-item>
+    <el-form-item label="Instance delivery">
+      <el-switch v-model="form.delivery"/>
+    </el-form-item>
+    <el-form-item label="Activity type" prop="type">
+      <el-checkbox-group v-model="ruleForm.type">
+        <el-checkbox label="Online activities" name="type" />
+        <el-checkbox label="Promotion activities" name="type" />
+        <el-checkbox label="Offline activities" name="type" />
+        <el-checkbox label="Simple brand exposure" name="type" />
+      </el-checkbox-group>
+    </el-form-item>
+    <el-form-item label="Resources" prop="resource">
+      <el-radio-group v-model="ruleForm.resource">
+        <el-radio label="Sponsorship" />
+        <el-radio label="Venue" />
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="Activity form" prop="desc">
+      <el-input v-model="ruleForm.desc" type="textarea" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)">
+        Create
+      </el-button>
+      <el-button>Cancel</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue';
-  let date = ref(new Date());
-  const changeDate = () => {
-    date.value = new Date(new Date().getTime() - (1 * 3600 * 24 * 1000));
-  }
+import { reactive } from "vue";
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type:[],
+  resource: '',
+  desc: '',
+});
+
+const onSubmit =() => {
+  console.log('submit!');
+}
 </script>
